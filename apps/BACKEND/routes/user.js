e6cf4af84118f47dff65f4bea050aec58d595732
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-01-21 04:10:25 +0530

    added routes

--- apps/BACKEND/routes/user.js
+++ apps/BACKEND/routes/user.js
@@ -1,47 +1,49 @@
-const { Router } = require("express");
+require("dotenv").config();
+const { Router } = require('express');
+const jwt = require('jsonwebtoken');
+const { User } = require('../db'); // Import User model from db/index.js
 const router = Router();
-const jwt = require("jsonwebtoken");
 
-
-// User Routes
+// User signup route
 router.post('/signup', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
+  
     await User.create({
-            username : username,
-            password : password
-    })
+        username : username,
+        password : password
+})
 
     res.json({
-        message : 'user created successfully'
+    message : 'user created successfully'
     })
-
 
 });
 
+// User signin route
 router.post('/signin', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    console.log(JWT_SECRET);
 
-    const user = await User.findOne({
-        username : username,
-        password : password
-    })
-    if (user) {
-        const token = jwt.sign({
-            username
-        }, JWT_SECRET);
+    
+        const user = await User.findOne({
+            username : username,
+            password : password
+        })
+        if (user) {
+            const token = jwt.sign({ 
+                username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.json({
-            token
-        })
-    } else {
-        res.status(411).json({
-            message: "Incorrect email and pass"
-        })
-    }
+                res.json({
+                    token
+                })
+                
+        } else {
+            res.status(411).json({
+                message: "Incorrect email and pass"
+            })
+        }
 });
 
 module.exports = router;
