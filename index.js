b0f3e5d7b96afe2e2e1b651ec507c03fd0f33869
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-01-21 04:11:41 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config(); 
+const { connectDB } = require('./apps/BACKEND/db'); 
+
+const adminRouter = require('./apps/BACKEND/routes/admin'); // Admin routes
+const userRouter = require('./apps/BACKEND/routes/user');   // User routes
+const { connect } = require('mongoose');
+
 const app = express();
-
-// Middleware for parsing request bodies
-app.use(bodyParser.json());
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
-
 const PORT = 1200;
+
+// Connect to MongoDB
+connectDB()
+
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/admin', adminRouter);
+app.use('/user', userRouter);
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
