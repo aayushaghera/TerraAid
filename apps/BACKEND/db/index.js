ecd1543b50f7d29321c8a299e1143e74904a7f08
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-01-21 04:05:09 +0530

    add DataBase

--- apps/BACKEND/db/index.js
+++ apps/BACKEND/db/index.js
@@ -1,24 +1,32 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
+const connectDB = async () => {
+
+        await mongoose.connect(process.env.CONNECTION_LINK)
+        
+    
+};
+
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://Db_Ngo:aayush123@cluster0.zpd4r.mongodb.net/login_cred');
+connectDB();
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
     username: String,
-    password: String
+    password: String,
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
     username: String,
     password: String,
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
+
 module.exports = {
     Admin,
-    User
-}
+    User,
+    connectDB
+};
