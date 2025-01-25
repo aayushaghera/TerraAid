993c149e24199ff1e94cf5b814eb54e5912eea49
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-01-25 08:02:38 +0530

    add paymentverifiction route

--- apps/BACKEND/Controllers/paymentController.js
+++ apps/BACKEND/Controllers/paymentController.js
@@ -1,83 +1,48 @@
-const { instance } = require('../../../razorpay.js');  
+const { instance } = require('../../../razorpay.js');
 const { Router } = require('express');
+const { Payment } = require('../db/index.js');
 const router = Router();
 
 
 router.post('/checkout', async (req, res) => {
+    const { amount } = req.body;
 
+    if (!amount) {
+        return res.status(400).json({
+            success: false,
+            message: "Amount is required",
+        });
+    }
 
     const options = {
-        amount: 50000,
+        amount: Number(amount) * 100, 
         currency: "INR",
     };
 
-        // Create an order using Razorpay's API
-        const order = await instance.orders.create(options);
-        console.log(order);
+    const order = await instance.orders.create(options);
 
-        res.status(200).json({
-            success: true,
-        });
+    res.status(200).json({
+        success: true,
+        order: order,
+    });
+});
+
+
+router.post('/paymentVerification', async (req, res) => {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+    
+    await Payment.create({
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature,
+    });
+
+    console.log(req.body);
+
+    res.status(200).json({
+        message: "Payment verified and details saved successfully",
+    });
 });
 
 module.exports = { router };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const { instance } = require('../../../razorpay.js');  // Adjust the path as needed
-// const { Router } = require('express');
-// const router = Router();
-
-// router.post('/checkout', async (req, res) => {
-//         console.log('Razorpay instance:', instance);
-
-//         const options = {
-//             amount: 50000,  
-//             currency: "INR",
-        
-//         };
-
-//         // Create an order using Razorpay's API
-//         const order = await instance.orders.create(options);
-//         console.log(order);
-//         res.status(200).json({
-//             success: true,
-//             order: order
-//         });
-    
-// });
-
-// // Export only the router
-// module.exports = { router };
-
