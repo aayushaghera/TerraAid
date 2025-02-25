a154555fc016a2a4c30a385dbe8e4b0c5592b524
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-02-25 20:13:57 +0530

    add Homepage

---
+++ apps/FRONTED/vite-project/src/Pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import NGOCategory from "../LandingPage/NGOCategory";
+import Header from "../Header/Header";
+import LandPage from "../LandingPage/LandPage";
+import Working from "../LandingPage/Working";
+import Testimonials from "../LandingPage/Testimonials";
+import Footer from "../Footer/Footer";
+
+function HomePage()
+{
+      return(
+        <div className="min-h-screen bg-mine-shaft-950 font-['Poppins']">
+
+
+            {/* //<Header></Header> */}
+            <LandPage></LandPage>
+            <NGOCategory></NGOCategory>
+            <Working></Working>
+            <Testimonials></Testimonials>
+            {/* <Footer></Footer> */}
+        </div>
+      )
+}
+
+export default HomePage;
