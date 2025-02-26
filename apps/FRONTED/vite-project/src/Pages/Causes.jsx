b98a6518d2847bf983346591ec1597efb212a364
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-02-27 00:27:29 +0530

    add CausesPage

---
+++ apps/FRONTED/vite-project/src/Pages/Causes.jsx
@@ -0,0 +1,16 @@
+
+import Ngo from "../Causes/Ngo";
+import SearchBar from "../Causes/SearchBar";
+import { Divider } from '@mantine/core';
+
+function Causes()
+{
+    return <div className="min-h-screen bg-mine-shaft-950 font-['poppins]">
+        <Divider color="#5a5a5a"size="xs" mx="md"/>
+        <SearchBar></SearchBar>
+        <Divider color="#5a5a5a" size="xs" mx="md"/>
+        <Ngo></Ngo>
+    </div>
+}
+
+export default Causes;
