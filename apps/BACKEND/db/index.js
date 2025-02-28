72f64c109058ebdc83c3ba93fd7fe6dbe10173a4
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-03-01 00:04:25 +0530

    Added Mongoose schemas for posts, NGOs, and contact details, including Razorpay-related fields.

--- apps/BACKEND/db/index.js
+++ apps/BACKEND/db/index.js
@@ -19,6 +19,7 @@ const AdminSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
+    pannumber : String
 });
 
 
@@ -37,14 +38,66 @@ const paymentSchema = new mongoose.Schema({
     },
   });
 
+const PostSchema = new mongoose.Schema({
+    Name: String,
+    location: String,
+    Goal: String,
+    description: String,
+    createdAt: { type: Date, default: Date.now },
+});  
+
+const ContactDetailsSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  contact: Number,
+  type: String,
+  reference_id: String,
+  notes: {
+    position: String,
+    comments: String,
+  },
+});
+
+const NGOSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  referenceId: String,
+  legalBusinessName: String,
+  businessType: String,
+  contactName: String,
+  address: {
+    street1: String,
+    street2: String,
+    city: String,
+    state: String,
+    postal_code: String,
+    country: String,
+  },
+  pan: String,
+  gst: String,
+  bankDetails: {
+    accountNumber: String,
+    ifsc: String,
+  },
+  razorpayAccountId: String,
+  fundAccountId: String,
+  contactId: String,
+  contactDetails: ContactDetailsSchema,
+});
+
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
 const Payment = mongoose.model("Payment", paymentSchema);
+const create_post = mongoose.model("post",PostSchema);
+const NGO = mongoose.model("NGO", NGOSchema);
 
 module.exports = {
     Admin,
     User,
     Payment,
+    create_post,
+    NGO,
     connectDB,
     
 };
