ff5bc086b2e418271b6586d1c6f2abc1a8c01923
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-03-13 15:10:00 +0530

    some changes in createpost page

--- apps/FRONTED/vite-project/src/Pages/CreatePost.jsx
+++ apps/FRONTED/vite-project/src/Pages/CreatePost.jsx
@@ -1,3 +1,4 @@
+
 import Post from '../CreatePost/Post'; // Renamed import
 import { Divider } from '@mantine/core';
 
@@ -6,6 +7,7 @@ function CreatePost() {
         <div className="min-h-screen bg-mine-shaft-950 font-['poppins']">
             <Divider color="#5a5a5a" size="xs" mx="md" />
             <Post /> 
+            
         </div>
     );
 }
